2344c0c4f4fb569b02ace7fe941bea05773bcfe2
Melonslise <arturzartdinov@gmail.com>
2022-12-07 12:59:06 +0300

    Day 7

---
+++ day7/part2.js
@@ -0,0 +1,156 @@
+const FS = require("fs");
+
+
+
+/* ----------- Helpers and data structures ----------- */
+
+Array.prototype.withMinLength = function(minLength)
+{
+	this.length = Math.max(this.length, minLength);
+	return this;
+}
+
+class TreeNode
+{
+	constructor(value, size)
+	{
+		this.parent = null;
+		this.value = value;
+
+		this.size = size || 0;
+
+		this.children = [];
+	}
+
+	isDir()
+	{
+		return this.size <= 0;
+	}
+
+	addChild(node)
+	{
+		if(this.getChild(node.value)) // tried inserting duplicate
+		{
+			return;
+		}
+
+		this.children.push(node);
+		node.parent = this;
+	}
+
+	getChild(value)
+	{
+		return this.children.find(n => n.value === value);
+	}
+
+	calcSize()
+	{
+		if(!this.isDir())
+		{
+			return this.size;
+		}
+
+		return this.children
+		.map(n => n.calcSize())
+		.reduce((acc, cur) => acc + cur);
+	}
+}
+
+class CommandMap
+{
+	constructor()
+	{
+		this.commands = {};
+	}
+
+	add(name, action)
+	{
+		this.commands[name] = action;
+	}
+
+	execute(command, arg, output)
+	{
+		/*
+		console.log("cmd:", command);
+		console.log("arg:", arg);
+		console.log("output:", output);
+		console.log("-------------------");
+		*/
+
+		const action = this.commands[command];
+
+		if(action)
+		{
+			action(arg, output);
+		}
+	}
+}
+
+
+
+/* ----------- Init ----------- */
+
+const root = new TreeNode("/");
+
+let currentNode;
+
+const commands = new CommandMap();
+commands.add("cd", arg =>
+{
+	switch(arg)
+	{
+	case "/":
+		currentNode = root;
+		break;
+	case "..":
+		currentNode = currentNode.parent;
+		break;
+	default:
+		currentNode = currentNode.getChild(arg);
+	}
+});
+commands.add("ls", (arg, output) =>
+{
+	for(const [ type, name ] of output)
+	{
+		const size = type === "dir" ? 0 : +type;
+		currentNode.addChild(new TreeNode(name, size));
+	}
+});
+
+
+
+/* ----------- Parsing and solution ----------- */
+
+FS.readFileSync("in.txt", { encoding: "utf-8" })
+.split("$ ")
+.filter(text => text != "")
+.map(lines => lines.trim()
+	.split("\r\n")
+	.map(line => line
+		.split(" ")
+		.withMinLength(2))) // ensure arg is always present, even if undefined
+.forEach( ([ query, ...output ]) => commands.execute(...query, output) );
+
+
+
+function getDirSizes(node, res)
+{
+	if(node.isDir())
+	{
+		res.push(node.calcSize());
+	}
+
+	node.children.forEach(n => getDirSizes(n, res));
+
+	return res;
+}
+
+const sizes = getDirSizes(root, []).sort((a, b) => a - b);
+
+const updateSize = 30000000;
+const totalSpace = 70000000;
+const rootSize = sizes.at(-1);
+const spaceNeeded = updateSize - (totalSpace - rootSize);
+
+console.log(sizes.find(size => size >= spaceNeeded));
